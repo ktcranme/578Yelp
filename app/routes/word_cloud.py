ae79222d3c23ae9b75347c06e30b71c4cae6489e
Ashish <ashish.mlr.123@gmail.com>
2020-04-23 00:13:13 -0700

    fomatting and comments (#6)

--- app/routes/word_cloud.py
+++ app/routes/word_cloud.py
@@ -1,13 +1,17 @@
 from flask import Blueprint, jsonify
 from app.mongo.reviews import Reviews
 from app.ml.nlp import WordCloud
-
 word_cloud_bp = Blueprint('word_cloud_api', __name__, url_prefix='/wordCloud')
+
 
 @word_cloud_bp.route('/testing', methods=['GET'])
 def testing():
-    
-    # fetch all data
+    """API to generate word cloud
+
+    Returns:
+        json -- words with weights and
+        sentiment color {'name':, 'weight':, 'color':}
+    """
     reviews = Reviews()
     f = {'business_id': 'XKOAi4J47i-YEhhHfKkPRQ'}
     res = reviews.getReviews(f=f, cols={'text': 1, 'stars': 1})
@@ -16,14 +20,15 @@ def testing():
         reviews.append(each['text'])
         stars.append(each['stars'])
 
-    # fetch word frequency
     cloud = WordCloud(docs=reviews, ratings=stars)
     word_count, word_color = cloud.getCounter()
 
-    # transform
     resp = []
     for index, each in enumerate(word_count.most_common(50)):
-        resp.append({'name': each[0], 'weight': each[1], 'color': word_color[each[0]]})
+        resp.append({
+            'name': each[0],
+            'weight': each[1],
+            'color': word_color[each[0]]
+        })
 
     return jsonify(resp), 200
-    
