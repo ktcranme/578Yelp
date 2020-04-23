e3e7d8d17482b4326cf77496434322102f6ed91e
Ashish <ashish.mlr.123@gmail.com>
2020-04-22 21:59:19 -0700

    adding color to word cloud

--- app/routes/word_cloud.py
+++ app/routes/word_cloud.py
@@ -2,26 +2,28 @@ from flask import Blueprint, jsonify
 from app.mongo.reviews import Reviews
 from app.ml.nlp import WordCloud
 
-
 word_cloud_bp = Blueprint('word_cloud_api', __name__, url_prefix='/wordCloud')
 
 @word_cloud_bp.route('/testing', methods=['GET'])
 def testing():
+    
     # fetch all data
     reviews = Reviews()
-    f = {}
-    f['date'] = {"$gte" : "2015-01-01 00:00:00" , "$lte" : "2015-01-01 1:00:00"}
-    res = reviews.getReviews(f=f)
-    res = [each['text'] for each in res]
+    f = {'business_id': 'IkMwuS3eViVHK09npuG1jQ'}
+    res = reviews.getReviews(f=f, cols={'text': 1, 'stars': 1})
+    reviews, stars = [], []
+    for each in res:
+        reviews.append(each['text'])
+        stars.append(each['stars'])
 
     # fetch word frequency
-    cloud = WordCloud(res)
-    word_frequencies = cloud.getCounter()
+    cloud = WordCloud(docs=reviews, ratings=stars)
+    word_count, word_color = cloud.getCounter()
 
     # transform
     resp = []
-    for each in word_frequencies.most_common(35):
-        resp.append({'name': each[0], 'weight': each[1], 'sentimentScore': 0.7212508438888889})
+    for index, each in enumerate(word_count.most_common(35)):
+        resp.append({'name': each[0], 'weight': each[1], 'color': word_color[each[0]]})
 
     return jsonify(resp), 200
     
