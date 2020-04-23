b9ccca87f42cb3812ff9f59296117e66038c8515
Ashish <ashish.mlr.123@gmail.com>
2020-04-23 00:12:42 -0700

    fomatting and comments

--- app/mongo/reviews.py
+++ app/mongo/reviews.py
@@ -1,10 +1,20 @@
 from app.mongo.yelp_client import YelpClient
 
+
 class Reviews(YelpClient):
     def __init__(self):
         super().__init__()
-    
+
     def getReviews(self, f={}, cols={}):
+        """Query reviews with filters and columns 
+
+        Keyword Arguments:
+            f {dict} -- filters to apply (default: {{}})
+            cols {dict} -- cols to exclude/include (default: {{}})
+
+        Returns:
+            cur -- query cursor
+        """
         cols['_id'] = 0
         res = list(self.getCollection('reviews').find(
             f,
@@ -12,10 +22,3 @@ class Reviews(YelpClient):
         ))
         self.closeConnection()
         return res
-
-    
-
-
-
-    
-    
