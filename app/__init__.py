03b617c4b2f69f8e840180e1ffb96b110c933dbb
Ashish <ashish.mlr.123@gmail.com>
2020-04-23 12:21:57 -0700

    adding local and prod config

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,24 @@
 from flask import Flask
 from app.routes import word_cloud
+import requests
+
+
+def is_prod():
+    r = requests.get('http://metadata.google.internal')
+    if r.headers.get('Metadata-Flavor', None) == 'Google':
+        return True
+    return False
 
 
 def create_app():
+
     # Construct the core application.
     app = Flask(__name__)
-    app.config.from_object('app.config')
+    if is_prod():
+        app.config.from_object('app.config.Prod')
+    else:
+        app.config.from_object('app.config.Local')
+
     with app.app_context():
         # Import parts of our application
         app.register_blueprint(word_cloud.word_cloud_bp)
--- app/config.py
+++ app/config.py
@@ -1,1 +1,8 @@
-DEBUG=True
+class Local(object):
+    DEBUG = True
+    PORT = 5000
+
+
+class Prod(object):
+    DEBUG = False
+    PORT = 8000
