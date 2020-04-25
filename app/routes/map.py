def34ed766c2f8c9c4a1d8114e04037e4834e6ce
Ashish <ashish.mlr.123@gmail.com>
2020-04-25 15:36:10 -0700

    fix for static

--- app/routes/map.py
+++ app/routes/map.py
@@ -5,7 +5,7 @@ map_bp = Blueprint('map_api', __name__, url_prefix='/mappath')
 
 @map_bp.route('/testing', methods=['GET'])
 def testing():
-    with open('../static/assets/mapData/2010data.csv') as f:
+    with open('app/static/assets/mapData/2010data.csv') as f:
         for line in f:
             print(line)
             return jsonify(line), 200
