9ff72f863007cdfa262ff179f8a5a8b1fe9efcfb
Ashish <ashish.mlr.123@gmail.com>
2020-04-25 03:06:55 -0700

    Wc (#9)
    * supplying business_id as query string
    
    * changing wc to ES6 class
    
    * formatting

--- app/static/custom/js/word_cloud.js
+++ app/static/custom/js/word_cloud.js
@@ -1,48 +1,74 @@
-// Creating empty chart
-var chart = Highcharts.chart('word-cloud-panel', {
-  accessibility: {
-    screenReaderSection: {
-      beforeChartFormat: '<h5>{chartTitle}</h5>'
-    }
-  },
-  series: [{
-    type: 'wordcloud',
-    data: [],
-    name: 'Occurrences',
-    minFontSize: 25,
-    maxFontSize: 100
-  }],
-  title: {
-    text: 'Review word cloud'
-  },
-  tooltip: {
-    formatter: function () {
-      return '<b>Name: </b>' + this.point.name + '<br>' +
-        '<b>Occurences: </b>' + this.point.weight + '<br>' +
-        '<b>Sentiment: </b>' + this.point.sentiment;
-    }
-  },
-  credits: { enabled: false }
-});
+class wordCloudChart {
+  constructor(business_id) {
+    this.divId = null;
+    this.chart = null;
+    this.data = null;
+    this.businessId = business_id
+  }
 
-chart.hideNoData();
-chart.showLoading();
+  // function to create 
+  createChart = () => {
+    // Creating empty chart
+    this.chart = Highcharts.chart('word-cloud-panel', {
+      accessibility: {
+        screenReaderSection: {
+          beforeChartFormat: '<h5>{chartTitle}</h5>'
+        }
+      },
+      series: [{
+        type: 'wordcloud',
+        data: [],
+        name: 'Occurrences',
+        minFontSize: 25,
+        maxFontSize: 100
+      }],
+      title: {
+        text: 'Review word cloud'
+      },
+      tooltip: {
+        formatter: function () {
+          return '<b>Name: </b>' + this.point.name + '<br>' +
+            '<b>Occurences: </b>' + this.point.weight + '<br>' +
+            '<b>Sentiment: </b>' + this.point.sentiment;
+        }
+      },
+      credits: { enabled: false }
+    });
 
-//  Fetching data
-fetch('/wordCloud/testing')
-  .then(res => {
-    chart.hideLoading();
-    return res.json();
-  })
-  .then(data => {
-    genWordCloud(data);
-  })
-  .catch(() => {
-    chart.showNoData("Error loading cloud");
-  });
+    //show loading data
+    this.chart.hideNoData();
+    this.chart.showLoading();
+  }
 
+  //fetch data
+  getWordCloud = () => {
+    fetch('/wordCloud/testing?business_id=' + this.businessId)
+      .then(res => {
+        this.chart.hideLoading();
+        return res.json();
+      })
+      .then(data => {
+        this.data = data;
+        this.redrawChart();
+      })
+      .catch(() => {
+        this.chart.showNoData("Error loading cloud");
+      });
+  }
 
-genWordCloud = (data) => {
-  chart.series[0].setData(data);
-  chart.redraw();
+  redrawChart = () => {
+    this.chart.series[0].setData(this.data);
+    this.chart.redraw();
+  }
+
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const businessId = 'XKOAi4J47i-YEhhHfKkPRQ';
+  let wcChart = new wordCloudChart(businessId);
+  wcChart.createChart();
+  wcChart.getWordCloud();
+  wcChart.redrawChart();
+});
