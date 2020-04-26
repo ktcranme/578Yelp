68504c4c96903474d6a10e5dff2e876fdb2a5173
ktcranme <ktcranme@asu.edu>
2020-04-25 17:21:02 -0700

    map has a listener for click now

--- app/routes/map.py
+++ app/routes/map.py
@@ -5,7 +5,8 @@ map_bp = Blueprint('map_api', __name__, url_prefix='/mappath')
 
 @map_bp.route('/testing', methods=['GET'])
 def testing():
+    businesses = []
     with open('app/static/assets/mapData/2010data.csv') as f:
         for line in f:
-            print(line)
-            return jsonify(line), 200
+            businesses.append(line.split(','))
+    return jsonify(businesses), 200
