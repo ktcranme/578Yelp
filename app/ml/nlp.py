0d046572e01ac7fc2d50dfb02a1d1032fa4e3b9d
Ashish <ashish.mlr.123@gmail.com>
2020-04-27 19:49:50 -0700

    adding nouns is propn count <= 5

--- app/ml/nlp.py
+++ app/ml/nlp.py
@@ -15,7 +15,7 @@ class WordCloud():
         self.docs = docs
         self.ratings = ratings
         self.disablelayers = ['parser', 'ner', 'textcat']
-        self.pos = ['PROPN']
+        self.pos = ['NOUN', 'PROPN']
         self.nlp = spacy.load('en', disable=self.disablelayers)
         self.color_range = ['#c70039', '#c70039',
                             '#ffd800', '#6fb98f', '#2b580c']
@@ -26,17 +26,27 @@ class WordCloud():
         Returns:
             tuple -- (list of word count dicts, list of word color dicts)
         """
-        word_count = Counter()
-        word_rating = defaultdict(list)
+        word_count, noun_word_count = Counter(), Counter()
+        word_rating, noun_word_rating = defaultdict(list), defaultdict(list)
         docs = self.nlp.pipe(
             self.docs, n_process=1, disable=self.disablelayers)
+        
 
         for index, doc in enumerate(docs):
             for token in doc:
                 if not token.is_stop and not token.is_punct and token.pos_ in self.pos:
-                    word_count[token.lemma_] += 1
-                    word_rating[token.lemma_].append(self.ratings[index])
+                    if token.pos_ == 'PROPN':
+                        word_count[token.lemma_] += 1
+                        word_rating[token.lemma_].append(self.ratings[index])
+                    else:
+                        noun_word_count[token.lemma_] += 1
+                        noun_word_rating[token.lemma_].append(self.ratings[index])
 
+        # if 0<=proper nouns<=5 found, add regular nouns
+        if not word_count or len(word_count) <= 5:
+            word_count += noun_word_count
+            word_rating = {**word_rating, **noun_word_rating}
+ 
         word_color = {word: self.getColor(
             ratings)[1] for word, ratings in word_rating.items()}
         word_sentiment = {word: self.getColor(
