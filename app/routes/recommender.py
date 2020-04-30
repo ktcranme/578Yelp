0baed754235bea649976466a22b97bbd3e5e4fcf
Ashish <ashish.mlr.123@gmail.com>
2020-04-29 18:27:11 -0700

    Hopefully final (#17)
    * new adds
    
    * added recommender API and button to trigger the API
    
    * adding render
    
    * added business id to return value of recommender API
    
    * added business_id to return of recommender API
    
    * updated for api
    
    * adding data2
    
    * fixing
    
    * Fixed recommender
    
    * adding panmap with on-click
    
    * adding stars and clean up
    
    * moving to static
    
    Co-authored-by: nishant <nishravinuthala@gmail.com>
    Co-authored-by: Siddharth Pandey <sidpandey12@gmail.com>

---
+++ app/routes/recommender.py
@@ -0,0 +1,56 @@
+from flask import Blueprint, jsonify, request
+from app.mongo.business import Business
+from app.mongo.checkins import Checkins
+from datetime import datetime
+from scipy.spatial import distance
+import pickle
+import numpy as np
+
+recommender_bp = Blueprint(
+    'recommender_api', __name__, url_prefix='/recommender')
+
+@recommender_bp.route('/testing', methods=['GET'])
+def recommender_entry():
+    categories = request.args.get('categories')    
+    category_list = [c for c in categories.split(',')]
+    inter = recommender(category_list)
+    resp = []
+    for val in inter:
+        inter_dict = {'name':val[0], 'business_id':val[4], 'latitude':val[2], 'longitude' : val[3], 'stars':val[1]}
+        resp.append(inter_dict)
+    return jsonify(resp), 200
+
+def recommender(category_list):
+    business_db = Business()
+    with open('app/static/data2.p', 'rb') as fp:
+        data_vector_dictionary = pickle.load(fp)
+    input_array = np.array([0] * len(data_vector_dictionary['pQeaRpvuhoEqudo3uymHIQ']))
+
+    with open("app/static/restaurant_categories.txt", "rb") as fp:
+	      b = pickle.load(fp)
+    distinct_categories = b[722:]
+
+    category_map = {}
+    for i in range(0,len(distinct_categories)):
+       category_map[distinct_categories[i]] = i
+
+    for cat in category_list:
+       input_array[category_map[cat]] = 1
+
+    topk = 10
+    restaurant_display_list = []
+    dist_list = []
+
+    for key in data_vector_dictionary:
+	    d = distance.euclidean(input_array, data_vector_dictionary[key])
+	    dist_list.append([key,d])
+    dist_list.sort(key=lambda x: x[1])
+
+    restaurants = business_db.getBusiness()
+    for i in range(0,topk):
+	    for val in restaurants:
+		    if (val['business_id'] == dist_list[i][0]):
+			    restaurant_display_list.append([val['name'],val['stars'],val['latitude'],val['longitude'],val['business_id']]) 
+
+    restaurant_display_list=sorted(restaurant_display_list, key = lambda x: x[1], reverse=True)  
+    return restaurant_display_list   
--- app/static/custom/js/recommender.js
+++ app/static/custom/js/recommender.js
@@ -1,8 +1,11 @@
+global_categories = []
+
 class recPanel {
   //initialize table and select button in panel
-  constructor () {
+  constructor() {
     this.recTable = null;
     this.multiSelect = null;
+    this.topRestaurants = [];
     this.selectOptions = ['Ethnic Food', 'Food Trucks', 'Specialty Food', 'Imported Food', 'Argentine', 'Food', 'Restaurants', 'Empanadas', 'Pretzels', 'Bakeries', 'Fast Food', 'Vietnamese', 'Soup', 'Burgers', 'Egyptian', 'Middle Eastern', 'Coffee & Tea', 'Cafes', 'Local Flavor', 'Pizza', 'Breakfast & Brunch', 'American (Traditional)', 'Salad', 'Thai', 'Fish & Chips', 'Seafood', 'Ice Cream & Frozen Yogurt', 'Sandwiches', 'Dive Bars', 'Comfort Food', 'Bars', 'Nightlife', 'Italian', 'Hot Dogs', 'Street Vendors', 'Active Life', 'Venues & Event Spaces', 'Mexican', 'Desserts', 'Sushi Bars', 'Chinese', 'Barbeque', 'Southern', 'American (New)', 'Chicken Wings', 'Japanese', 'Dim Sum', 'Vegetarian', 'Ramen', 'Vegan', 'Food Stands', 'Shaved Ice', 'Themed Cafes', 'Shaved Snow', 'Bubble Tea', 'Juice Bars & Smoothies', 'Coffee Roasteries', 'Tapas Bars', 'Gastropubs', 'Cocktail Bars', 'Lounges', 'Wine Bars', 'Internet Cafes', 'Gluten-Free', 'Fruits & Veggies', 'Food Delivery Services', 'Mediterranean', 'Asian Fusion', 'Pubs', 'Sports Bars', 'Beer', 'Wine & Spirits', 'Social Clubs', 'Signature Cuisine', 'Hookah Bars', 'Tex-Mex', 'Fondue', 'Wraps', 'Delis', 'Gelato', 'Tacos', 'Greek', 'African', 'Hotel bar', 'Korean', 'Buffets', 'French', 'Steakhouses', 'Caribbean', 'Breweries', 'Beer Bar', 'Tapas/Small Plates', 'Szechuan', 'Taiwanese', 'Bagels', 'Grocery', 'Indian', 'Noodles', 'Malaysian', 'Teppanyaki', 'Hawaiian', 'Pasta Shops', 'Creperies', 'Cajun/Creole', 'Wineries', 'Falafel', 'German', 'Slovakian', 'Hotels', 'British', 'Cideries', 'Pan Asian', 'Casinos', 'Conveyor Belt Sushi', 'Food Court', 'Cinema', 'Donuts', 'Irish', 'Irish Pub', 'Lebanese', 'Acai Bowls', 'Waffles', 'Tea Rooms', 'Community Service/Non-Profit', 'Soul Food', 'Cheesesteaks', 'Filipino', 'Cantonese', 'Ethiopian', 'Brewpubs', 'Custom Cakes', 'Cupcakes', 'Turkish', 'Diners', 'Indoor Playcentre', 'Latin American', 'Dinner Theater', 'Cafeteria', 'Puerto Rican', 'Spanish', 'Dance Clubs', 'Persian/Iranian', 'Chocolatiers & Shops', 'Macarons', 'Moroccan', 'Poke', 'Speakeasies', 'Beer Gardens', 'Modern European', 'Wine Tasting Room', 'Jazz & Blues', 'Tuscan', 'Brazilian', 'Hot Pot', 'Polish', 'Pakistani', 'Izakaya', 'Brasseries', 'Hong Kong Style Cafe', 'Halal', 'Eatertainment', 'Salvadoran', 'Colombian', 'Do-It-Yourself Food', 'Armenian', 'Laotian', 'Live/Raw Food', 'Piano Bars', 'Comedy Clubs', 'New Mexican Cuisine', 'Dominican', 'Bistros', 'Hungarian', 'Patisserie/Cake Shop', 'Food Banks', 'Popcorn Shops', 'Whiskey Bars', 'Cambodian', 'Venezuelan', 'Himalayan/Nepalese', 'Bangladeshi', 'Herbs & Spices', 'Bed & Breakfast', 'Soba', 'Kebab', 'Japanese Curry', 'Eritrean', 'Smokehouse', 'Farmers Market', 'Kosher', 'Peruvian', 'Portuguese', 'Basque', 'Singaporean', 'Pancakes', 'Cuban', 'Mongolian', 'Arabian', 'DJs', 'Ukrainian', 'Russian', 'Honduran', 'Calabrian', 'Scandinavian', 'Candy Stores', 'Cheese Shops', 'Nicaraguan', 'Grill Services', 'Belgian', 'Botanical Gardens', 'Unofficial Yelp Events', 'Shanghainese', 'Police Departments', 'Tiki Bars', 'Outlet Stores', 'Afghan', 'Game Meat', 'Guamanian', 'Supper Clubs', 'Pop-Up Restaurants', 'Airport Lounges', 'Bar Crawl', 'Honey', 'Chimney Cakes', 'Poutineries', 'Trinidadian', 'Gay Bars', 'Burmese', 'Water Stores', 'Nutritionists', 'Pumpkin Patches', 'Pick Your Own Farms', 'Farms', 'Indonesian', 'Cigar Bars', 'Syrian', 'Bartenders', 'Distilleries', 'Pub Food', 'Czech', 'Polynesian', 'Champagne Bars', 'Pool & Billiards', 'Kombucha', 'Sicilian', 'Delicatessen', 'Cabaret', 'Uzbek', 'Strip Clubs', 'Ski Resorts', 'Wine Tasting Classes', 'Tasting Classes', 'Health Retreats', 'Rotisserie Chicken', 'Club Crawl', 'Airport Terminals', 'Beach Bars', 'Cheese Tasting Classes', 'Wine Tours', 'Beer Garden', 'Coffeeshops', 'Beer Tours', 'Estate Liquidation', 'Catalan', 'Austrian', 'Oaxacan', 'South African', 'Iberian', 'Pita', 'Coffee & Tea Supplies', 'Service Stations', 'Hainan', 'Meaderies'];
   }
 
@@ -10,7 +13,7 @@ class recPanel {
   drawTable = () => {
     this.recTable = $('#rec-table').DataTable({
       data: [],
-      columns: [{ data: 'name' }, { data: 'test' }],
+      columns: [{ data: 'name' }, { data: 'stars' }],
       searching: false,
       scrollY: '50vh',
       bInfo: false,
@@ -19,13 +22,35 @@ class recPanel {
       language: { emptyTable: 'Please add/change preferences or map view' },
       paging: false,
       ordering: false,
-      processing: true
+      processing: true,
+      columnDefs: [
+        { width: "50%", "targets": 0 }
+      ]
+    });
+  }
+
+  //create entries in top 10 table after each category change
+  populateTable = () => {
+    //clear and redraw table
+    this.recTable.clear().draw();
+    this.recTable.rows.add(this.topRestaurants);
+    this.recTable.draw();
+    console.log(this.topRestaurants.length);
+    //populate stars
+    this.topRestaurants.forEach(restaurant=>{
+      var business_id = restaurant.business_id;
+      $(`#${restaurant.business_id}`).rateYo({
+        rating: restaurant.starRating,
+        readOnly: true
+      });
     });
   }
 
   //when top 10 restaurant is clicked, move map
   restaurantClick = business_id => {
     console.log(business_id);
+    var business = this.topRestaurants.filter(restaurant => restaurant.business_id === business_id)[0];
+    panMap(business.lat, business.long);
   }
 
   //render category select button
@@ -42,33 +67,41 @@ class recPanel {
       options: arr,
       plugins: ['remove_button'],
       placeholder: 'Type in your interests',
+      copyClassesToDropdown: false,
       onChange: val => this.getTopRestaurants(val)
     });
   }
 
   // get the top restaurants from API on select change
   getTopRestaurants = categories => {
-    console.log(categories);
-    var test = [];
-    categories.forEach(cat => {
-      test.push({
-        name:
-          '<a href="#" onclick="panelObject.restaurantClick(\'' +
-          cat +
-          '\')">' +
-          cat +
-          '</a>',
-        id: cat,
-        test: cat
+    if(!categories.length){ this.recTable.clear().draw(); }
+    fetch('/recommender/testing?categories=' + categories)
+      .then(res => {
+        return res.json()
       })
-    });
-    this.recTable.clear().draw();
-    this.recTable.rows.add(test);
-    this.recTable.draw();
+      .then(data => {
+        this.topRestaurants = [];
+        data.forEach(restaurant => {
+          this.topRestaurants.push({
+            name: `<a href="#" onclick="panelObject.restaurantClick('${restaurant.business_id}')">${restaurant.name}</a>`,
+            business_id: restaurant.business_id,
+            stars: `<div style="text-align: center;margin: 0 auto;" id=${restaurant.business_id}></div>`,
+            starRating: restaurant.stars,
+            lat: restaurant.latitude,
+            long: restaurant.longitude
+          });
+        });
+        this.populateTable();
+      })
+      .catch((exception) => {
+        console.log(exception);
+      });
   }
+
 }
 
 //draw panels with select and table
+
 document.addEventListener('DOMContentLoaded', () => {
   panelObject = new recPanel();
   panelObject.drawTable();
